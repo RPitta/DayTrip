f3a107aa70d6f610fd2f65fa2cb9be5266d7452e
Rodrigo P <eskimorhcp@gmail.com>
2022-12-09 20:57:09 -0500

    add date visited for reviews

--- models/review.js
+++ models/review.js
@@ -1,3 +1,4 @@
+const { string } = require('joi');
 const mongoose = require('mongoose')
 const Place = require('./place');
 const Schema = mongoose.Schema;
@@ -7,6 +8,7 @@ const Schema = mongoose.Schema;
 const ReviewSchema = new Schema({
     body: String,
     time: { type: Date, default: Date.now },
+    dateVisited: Date,
     city: String,
     state: String,
     useful: { type: Number, default: 0 },
@@ -23,29 +25,11 @@ const ReviewSchema = new Schema({
     ]
 })
 
-// TODO: Review body cannot be empty
 ReviewSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
 
-        // await Place.deleteMany({
-        //     $and: [
-        //         {
-        //             _id: {
-        //                 $in: doc.places
-        //             }
-        //         }
-        //         ,
-        //         {
-        //             recommendations: {
-        //                 $eq: 1
-        //             }
-        //         }
-        //     ]
-
-        // })
-
+        // TODO: When grabbing user favorites do a top 10 with a min value of 1
         await Place.updateMany(
-
             {
                 _id: {
                     $in: doc.places
@@ -60,8 +44,6 @@ ReviewSchema.post('findOneAndDelete', async function (doc) {
                 }
 
             }
-
-
         )
 
     }
